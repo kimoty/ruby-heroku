da8987ff90a6e5c7845b2116a5bd39cf987ca275
ryoji kimoto <ryoji.kimoto@gmail.com>
2012-04-21 08:26:42 +0900

    use DataMapper

--- index.rb
+++ index.rb
@@ -1,9 +1,25 @@
 require 'rubygems'
 require 'sinatra'
+require 'sequel'
+require 'dm-core'
+require 'dm-migrations'
+
+DataMapper::setup(:default, ENV['DATABASE_URL'] || 'sqlite3://db.sqlite3')
+
+
 
 configure :production do
 
 end
+
+
+#class Todo
+#	include DataMapper::Resource
+#	property :id, Serial
+#	property :todo, Text
+#	auto_upgrade!
+#end
+
 
 get '/' do
   #"hello"
